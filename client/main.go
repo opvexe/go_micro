e0921af59c56da55fc556d84fa2031a5a14e7a1f
SHUMIN <tao_shuminx@foxmail.com>
2020-01-19 12:48:21 +0800

    提交

--- client/main.go
+++ client/main.go
@@ -6,24 +6,24 @@ import(
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-plugins/registry/etcdv3"
-	pb "ibook/server/book/proto/book"
+	pbw "ibook/server/book/proto/book"
 )
 
 func main() {
-
 	//注册etcd
 	/* reg :=etcdv3.NewRegistry() */
 	reg := etcdv3.NewRegistry(func(op *registry.Options){
 		op.Addrs = []string{
-			"http://172.19.0.4:2379",
+			"http://127.0.0.1:32779",
 		}
 	})
 
 	sevice := micro.NewService(
 		micro.Registry(reg),
 	)
-	client := pb.NewGreeterService("go.micro.srv.book",sevice.Client())
-	resp,err:=client.SayHello(context.Background(),&pb.HelloRequest{
+	sevice.Init()
+	c := pbw.NewGreeterService("go.micro.srv.book",sevice.Client())
+	resp,err:=c.SayHello(context.Background(),&pbw.HelloRequest{
 		Name:                 "SHUMIN",
 	})
 	if err!=nil {
